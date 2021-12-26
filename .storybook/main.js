75ba07abe5a587a1f27b27e74a9d11fc7b648e38
Vasha Faris <vashafaris@gmail.com>
2021-12-26 14:59:51 +0700

    chore: setup storybook builder with webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+  typescript: { reactDocgen: false },
+};
